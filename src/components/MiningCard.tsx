45cecbff9e669ad787886a2bb6799b9521756b0c
brainauto012 <156060037+brainauto012@users.noreply.github.com>
2025-04-12 21:54:11 +0900

    fix: remove unused axios import

--- src/components/MiningCard.tsx
+++ src/components/MiningCard.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Button } from './ui/Button';
 import { motion } from 'framer-motion';
 import { FaCoins } from 'react-icons/fa';
